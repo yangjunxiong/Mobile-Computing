fe561e3600499c9197740bb19897e0c6f7380b61
liuziang668 <liuziang668@gmail.com>
2017-04-06 01:23:21 +0800

    Map modified
    改了需求，现在卡片可以自己出现。

--- Assets/Scripts/Map/CollectIcon.cs
+++ Assets/Scripts/Map/CollectIcon.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class getRandomCard : MonoBehaviour {
+public class CollectIcon : MonoBehaviour {
     public Sprite pencile;
     public Sprite cabinet;
     public Sprite desk;
@@ -14,78 +14,84 @@ public class getRandomCard : MonoBehaviour {
     public Sprite bomb;
     public Sprite reaper;
 
-    private Image card;
-    public Text numCards;
-
+    public int cardNo;
     public string itemName;
     public int itemNum;
 
+    public GetRandomIcon getIcon;
+    public Image card;
+    public GameObject cardObject;
+    public Text numCardsText;
+
     // Use this for initialization
     void Start () {
-        updateCard();
-    }
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 
-    }
-
-    public void updateCard()
+    void OnMouseUpAsButton()
     {
-        card = GetComponent<Image>();
-        System.Random rnd = new System.Random();
-        System.Random rndNum = new System.Random();
-        int random = rnd.Next(80);
+        //print("hello");
+        getIcon.turnOff();
+        cardNo = getIcon.randomCard;
 
-        if (random < 20)
+        cardObject.SetActive(true);
+
+        System.Random rndNum = new System.Random();
+
+        if (cardNo == 1)
         {
             card.sprite = pencile;
             itemName = "pencile";
             itemNum = rndNum.Next(1, 6);
         }
-        else if (random < 30)
+        else if (cardNo == 2)
         {
             card.sprite = cabinet;
             itemName = "cabinet";
             itemNum = rndNum.Next(1, 4);
         }
-        else if (random < 35)
+        else if (cardNo == 3)
         {
             card.sprite = desk;
             itemName = "desk";
             itemNum = rndNum.Next(1, 3);
         }
-        else if (random < 40)
+        else if (cardNo == 4)
         {
             card.sprite = perfume;
             itemName = "perfume";
             itemNum = rndNum.Next(1, 4);
         }
-        else if (random < 60)
+        else if (cardNo == 5)
         {
             card.sprite = scroll;
             itemName = "scroll";
             itemNum = rndNum.Next(1, 6);
         }
-        else if (random < 70)
+        else if (cardNo == 6)
         {
             card.sprite = book;
             itemName = "book";
             itemNum = rndNum.Next(1, 4);
         }
-        else if (random < 75)
+        else if (cardNo == 7)
         {
             card.sprite = bomb;
             itemName = "bomb";
             itemNum = rndNum.Next(1, 3);
         }
-        else if (random < 80)
+        else if (cardNo == 8)
         {
             card.sprite = reaper;
             itemName = "reaper";
             itemNum = rndNum.Next(1, 3);
         }
 
-        numCards.text = itemNum + "";
-    } 
+        numCardsText.text = itemNum + "";
+    }
 }
---
+++ Assets/Scripts/Map/GetRandomIcon.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetRandomIcon : MonoBehaviour {
+    public int randomCard;
+    public GameObject capsule;
+    public GameObject pencile;
+    public GameObject cabinet;
+    public GameObject desk;
+    public GameObject perfume;
+    public GameObject scroll;
+    public GameObject book;
+    public GameObject bomb;
+    public GameObject reaper;
+
+    public float timeLeft;
+    public int maxWaitTime = 60;
+
+    public bool hasShown = false;
+
+    public float positionx;
+    public float positionz;
+
+    // Use this for initialization
+    void Start () {
+        System.Random rnd = new System.Random();
+        timeLeft = rnd.Next(1, maxWaitTime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasShown)
+        {
+            return;
+        }
+
+        timeLeft -= UnityEngine.Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            System.Random rnd1 = new System.Random();
+            randomCard = rnd1.Next(1, 9);
+
+            System.Random rndx = new System.Random();
+            positionx = capsule.transform.position.x + rndx.Next(-8, 8);
+            
+
+            System.Random rndz = new System.Random();
+            positionz = capsule.transform.position.z + rndz.Next(-2, 10);
+            //print("randomCard: " + randomCard);
+            //print(positionx + " " + positionz);
+
+            if (randomCard == 1)
+            {
+                pencile.SetActive(true);
+                pencile.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 2)
+            {
+                cabinet.SetActive(true);
+                cabinet.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 3)
+            {
+                desk.SetActive(true);
+                desk.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 4)
+            {
+                perfume.SetActive(true);
+                perfume.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 5)
+            {
+                scroll.SetActive(true);
+                scroll.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 6)
+            {
+                book.SetActive(true);
+                book.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 7)
+            {
+                bomb.SetActive(true);
+                bomb.transform.position = new Vector3(positionx, 1, positionz);
+            }
+            else if (randomCard == 8)
+            {
+                reaper.SetActive(true);
+                reaper.transform.position = new Vector3(positionx, 1, positionz);
+            }
+
+            hasShown = true;
+        }
+    }
+
+    public void turnOff()
+    {
+        if (!hasShown)
+        {
+            return;
+        }
+        hasShown = false;
+
+        pencile.SetActive(false);
+        cabinet.SetActive(false);
+        desk.SetActive(false);
+        perfume.SetActive(false);
+        scroll.SetActive(false);
+        book.SetActive(false);
+        bomb.SetActive(false);
+        reaper.SetActive(false);
+
+        System.Random rnd = new System.Random();
+        timeLeft = rnd.Next(1, maxWaitTime);
+        //print("TimeLeft: "+timeLeft);
+    }
+
+}
